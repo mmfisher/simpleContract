e80dd1edcc5e56fb415059536f95df6163248703
mmfisher <781870368@qq.com>
2024-11-28 11:07:22 +0800

    view

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -4,10 +4,14 @@ pragma solidity ^0.8.8;
 
 contract SimpleStorage { 
     //boolean, uint, int, address, bytes
-    uint256 public favouriteNamber ;
+    uint256 public favouriteNumber ;
 
     function store(uint256 _favouriteNumber) public {
-        favouriteNamber = _favouriteNumber;
+        favouriteNumber = _favouriteNumber;
+    }
+
+    function viewFavouriteNumber() public view returns (uint256){
+        return favouriteNumber;
     }
 }
 //0xd9145CCE52D386f254917e481eB44e9943F39138
