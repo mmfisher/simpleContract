40e39e7156e0a2a2caba94fa67854af47120037e
mmfisher <781870368@qq.com>
2024-12-03 22:48:06 +0800

    import diractely from github

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,10 +3,29 @@ pragma solidity ^0.8.8;
 
 contract FundMe{
 
+    import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
     uint256 public minimumUsd = 50;
 
     function fund() public payable {
         // want to be able to set a minimun fund 
         require(msg.value > minimumUsd , "Didn`t sent enough!");
+    }
+
+    function getPrice() public {
+        // ABI
+        // Address  0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //feed addresses->price feed addresses -> sepolia testnet
+
+    }
+
+    function getVersion() public view return (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getConversionRate(){
+
     }
 }
